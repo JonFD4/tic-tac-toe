85b268ec6aab377084359eddf67cce23959c7212
Jonathan Forson <jonathanforsondanso@gmail.com>
2025-07-24 23:27:33 +0000

    Create get_player_move and playgame functions

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-board = np.zeros((3,3), dtype = int)
 
 
-# Convert to symbol
+
+# helper function that Convert numbers to symbols
 def get_symbol(cell):
     if cell == 1:
         return 'X'
@@ -12,9 +12,6 @@ def get_symbol(cell):
     else:
         return ' '
 
-print(get_symbol(1))
-print(get_symbol(-1))
-print(get_symbol(0))
 
 # print board
 def print_board(board):
@@ -24,4 +21,43 @@ def print_board(board):
         if row_index < 2:
             print("----+---+----")
 
-print_board(board)
+
+
+# get player move
+def get_player_move(player, board):
+    while True:
+        print(f"Player{get_symbol(player)}'s turn. ")
+        try:
+            row= int(input('Enter row(0,1,2): '))
+            col= int(input('Enter column(0,1,2): '))
+        except valueError:
+            print('Please, enter numbers only.')
+            continue
+        
+        if row not in [0,1,2] or col not in  [0,1,2]:
+            print('Invalid Position, Try again')
+            continue
+        if board[row,col]!=0:
+            print('That spot is taken. Try another')
+            continue
+
+        board[row,col]=player
+        print(f"Player {get_symbol(player)} placed at ({row}, {col})")
+
+        break
+    
+
+
+def play_game():
+    board = np.zeros((3,3), dtype = int)
+    current_player = 1
+
+    while True:
+        print_board(board)
+        get_player_move(current_player, board)
+
+        print_board(board)
+        current_player = -current_player
+        break
+
+play_game()
